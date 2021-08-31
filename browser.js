3610fbafc058697603ac643d5979f0a2a1511c3c
mganeko <masashi.g@gmail.com>
2021-08-31 23:02:13 +0900

    arg for servicename

--- browser.js
+++ browser.js
@@ -1,4 +1,4 @@
-// Simple googlezone browser
+// Simple mdns browser
 //   using node-mdns-js
 //     github https://github.com/mdns-js/node-mdns-js
 //     npm https://www.npmjs.com/package/mdns-js
@@ -7,7 +7,7 @@
 //    npm isntall mdns-js
 //
 // usage
-//    node browser.js
+//    node browser.js servicename
 
 
 const mdns = require('mdns-js');
@@ -20,8 +20,17 @@ const TIMEOUT = 5000; //5 seconds
 
 //const browser = mdns.createBrowser(); //defaults to mdns.ServiceType.wildcard
 //const browser = mdns.createBrowser(mdns.tcp('googlecast'));
-const browser = mdns.createBrowser(mdns.tcp('googlezone'));
+//const browser = mdns.createBrowser(mdns.tcp('googlezone'));
 
+let browser = null;
+if (process.argv.length > 2) {
+  const service = process.argv[2];
+  console.warn('service name =', service)
+  browser = mdns.createBrowser(mdns.tcp(service));
+}
+else {
+  browser = mdns.createBrowser();
+}
 
 browser.on('ready', function onReady() {
   console.warn('browser is ready');
